bfe75cd5bb50bd17208dd8a429d6586c60f73920
mmuneeburahman <56772228+mmuneeburahman@users.noreply.github.com>
2021-07-30 16:38:59 +0500

    nextSequence() function added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-alert("Hello");
+$(document).keypress(function(){
+  $("#level-title").text("Level 1");
+  nextSequence();
+})
 
-console.log("Hello");
+var buttonColors = ["red", "blue", "green", "yellow"]
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = [];
+    randomChosenColor.push(buttonColors[randomNumber]);
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+
+    console.log("sounds/"+randomChosenColor+".mp3");
+    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    audio.play();
+    console.log(randomChosenColor);
+}
+
